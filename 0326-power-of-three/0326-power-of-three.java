c25ad17b99a521d874e04531f457460eab7d8590
Pawan Pandey <pawanpandey.3002@gmail.com>
2025-08-14 15:14:26 +0530

    Time: 8 ms (84.4%), Space: 44.4 MB (27.89%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,7 +1,13 @@
-public class Solution {
+class Solution {
     public boolean isPowerOfThree(int n) {
-        if (n == 1) return true;
-        if (n <= 0 || n % 3 != 0) return false;
-        return isPowerOfThree(n / 3);
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n % 3 == 0) {
+            n = n / 3;
+        }
+
+        return n == 1;
     }
 }
