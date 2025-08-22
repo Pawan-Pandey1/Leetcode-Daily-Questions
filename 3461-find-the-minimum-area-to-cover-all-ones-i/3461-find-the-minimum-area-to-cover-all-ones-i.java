62a918cd4c7203f9cf8a3e29f1a93336e3162281
Pawan Pandey <pawanpandey.3002@gmail.com>
2025-08-22 23:59:12 +0530

    Time: 5 ms (94.31%), Space: 196.8 MB (61.79%) - LeetHub

---
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumArea(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int startRow = m, endRow = -1;
+        int startCol = n, endCol = -1;
+
+        for (int i = 0; i < m; i++) {     
+            for (int j = 0; j < n; j++) {  
+                if (grid[i][j] == 1) {     
+                    startRow = Math.min(startRow, i);
+                    endRow = Math.max(endRow, i);
+                    startCol = Math.min(startCol, j);
+                    endCol = Math.max(endCol, j);
+                }
+            }
+        }
+
+        return (endRow - startRow + 1) * (endCol - startCol + 1);
+    }
+}
